aa95316546d1456a464fc6d6dacd5c4e5b9d10a1
imanechrj <cherrajimane@icloud.com>
2024-03-27 16:40:01 +0100

    third part

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    HousingLocationComponent,
+    CommonModule],
   template: `
   <section>
     <form>
@@ -11,7 +15,10 @@ import { Component } from '@angular/core';
       <button class="primary" type="button">Search</button>
     </form>
   </section>
-`,
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
