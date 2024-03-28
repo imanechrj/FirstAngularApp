8f602b51345e8de586f5049a6f828dda17475511
imanechrj <cherrajimane@icloud.com>
2024-03-28 16:17:26 +0100

    Lesson 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
+    RouterModule
   ],
   template: `
   <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/img/logo.png" alt="logo" aria-hidden="true">
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/img/logo.png" alt="logo" aria-hidden="true">
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
 `,
+
   styleUrl: './app.component.css'
 })
 export class AppComponent {
